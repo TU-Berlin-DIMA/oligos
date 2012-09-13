da5991936c76d2e00d51beaeb869a4c6bcf066c4
Christoph Bruecke <christoph.bruecke@campus.tu-berlin.de>
2012-09-13 16:05:57 -0400

    Fixed warning: unused import.

--- src/main/java/de/tu_berlin/dima/oligos/exception/ColumnDoesNotExistException.java
+++ src/main/java/de/tu_berlin/dima/oligos/exception/ColumnDoesNotExistException.java
@@ -1,15 +1,10 @@
 package de.tu_berlin.dima.oligos.exception;
 
-import com.ibm.db2.jcc.am.co;
-
 public class ColumnDoesNotExistException extends Exception {
   
   private final String table;
   private final String column;
 
-  /**
-   * 
-   */
   private static final long serialVersionUID = 1L;
   
   public ColumnDoesNotExistException(String table, String column) {
