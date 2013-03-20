fa8c82ba6590a1855d1f7471a07b8ba7d30bbff6
Christoph Bruecke <christoph.bruecke@campus.tu-berlin.de>
2013-03-20 17:55:50 +0100

    Removed unused default from BigDecimalParser.

--- src/main/java/de/tu_berlin/dima/oligos/type/util/parser/BigDecimalParser.java
+++ src/main/java/de/tu_berlin/dima/oligos/type/util/parser/BigDecimalParser.java
@@ -1,12 +1,9 @@
 package de.tu_berlin.dima.oligos.type.util.parser;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 public class BigDecimalParser extends AbstractParser<BigDecimal> {
   
-  private final static int DEFAULT_SCALE = 2;
-
   @Override
   public BigDecimal fromString(String value) {
     return new BigDecimal(removeQuotes(value));
